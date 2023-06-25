e7abca245bdbbf31cc8f8bfb61b99d6eba5fba17
Julius Padi <87507466+pjmawuli@users.noreply.github.com>
2023-06-25 16:59:45 +0000

    Finally created the stupid search bar

--- app/src/main/java/com/theta/rarecipe/HomePageActivity.java
+++ app/src/main/java/com/theta/rarecipe/HomePageActivity.java
@@ -1,8 +1,8 @@
 package com.theta.rarecipe;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.SearchView;
 
 public class HomePageActivity extends AppCompatActivity {
 
@@ -10,5 +10,11 @@ public class HomePageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SearchView searchView = findViewById(R.id.search_view);
+        searchView.setIconifiedByDefault(false);
+
+        // Set the query hint text programmatically
+        searchView.setQueryHint("Search recipe");
     }
 }
