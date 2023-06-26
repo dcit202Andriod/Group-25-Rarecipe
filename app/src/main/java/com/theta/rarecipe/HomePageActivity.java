701e9dab9c3580897c1c1dee2cd8eeefe98367e0
Julius Padi <87507466+pjmawuli@users.noreply.github.com>
2023-06-26 19:11:34 +0000

    Created everything

--- app/src/main/java/com/theta/rarecipe/HomePageActivity.java
+++ app/src/main/java/com/theta/rarecipe/HomePageActivity.java
@@ -9,7 +9,7 @@ public class HomePageActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home_page);
 
         SearchView searchView = findViewById(R.id.search_view);
         searchView.setIconifiedByDefault(false);
