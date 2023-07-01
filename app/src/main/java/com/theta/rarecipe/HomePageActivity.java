1302728a8400779f0e24b30c38cd396396db8043
DyeDrake <aboateng_clifford@st.ug.edu.gh>
2023-07-01 15:07:42 +0000

    first inpute box completed

--- app/src/main/java/com/theta/rarecipe/HomePageActivity.java
+++ app/src/main/java/com/theta/rarecipe/HomePageActivity.java
@@ -10,5 +10,6 @@ public class HomePageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setContentView(R.layout.create_account);
     }
 }
