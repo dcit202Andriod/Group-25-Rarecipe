20113e48122a3161080e79e0e312ec8c80c35160
Julius Padi <87507466+pjmawuli@users.noreply.github.com>
2023-07-01 18:24:21 +0000

    Almost done with the trending section

---
+++ app/src/main/java/com/theta/rarecipe/FoodItem.java
@@ -0,0 +1,25 @@
+package com.theta.rarecipe;
+
+public class FoodItem {
+    private String name;
+    private String imageUrl;
+    private String creatorName;
+
+    public FoodItem(String name, String imageUrl, String creatorName) {
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.creatorName = creatorName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getCreatorName() {
+        return creatorName;
+    }
+}
