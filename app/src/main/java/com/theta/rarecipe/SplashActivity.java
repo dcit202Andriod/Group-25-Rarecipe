ec2f8876fb8af24440ac3d5db21e6d4595407c9e
DyeDrake <aboateng_clifford@st.ug.edu.gh>
2023-07-05 13:39:36 +0000

    This is a commit before final push for merge

--- app/src/main/java/com/theta/rarecipe/SplashActivity.java
+++ app/src/main/java/com/theta/rarecipe/SplashActivity.java
@@ -11,15 +11,13 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash_screen);
-
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run(){
-                Intent intent = new Intent(SplashActivity.this, HomePageActivity.class);
+                Intent intent = new Intent(SplashActivity.this, CreateAccount.class);
                 startActivity(intent);
                 finish();
             }
-        }, 3000);
+        }, 2050);
     }
 }
