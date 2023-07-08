4b5d1f7107cf781738863729a485b82e1d83b605
DyeDrake <aboateng_clifford@st.ug.edu.gh>
2023-07-08 20:56:23 +0000

    another commit

--- app/src/main/java/com/theta/rarecipe/SignInActivity.java
+++ app/src/main/java/com/theta/rarecipe/SignInActivity.java
@@ -4,15 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-<<<<<<<< HEAD:app/src/main/java/com/theta/rarecipe/MainActivity.java
-public class MainActivity extends AppCompatActivity {
-========
-public class LoginPageActivity extends AppCompatActivity {
->>>>>>>> Login:app/src/main/java/com/theta/rarecipe/LoginPageActivity.java
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+public class SignInActivity extends AppCompatActivity {
+
+    public class LoginPageActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.sign_in_activity);
+        }
     }
 }
