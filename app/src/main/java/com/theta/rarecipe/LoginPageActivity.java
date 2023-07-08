b5adff1d9cb16cc634187bfdbf28c478ac802335
Julius Padi <87507466+pjmawuli@users.noreply.github.com>
2023-07-08 15:54:49 +0000

    Changed background color

--- app/src/main/java/com/theta/rarecipe/LoginPageActivity.java
+++ app/src/main/java/com/theta/rarecipe/LoginPageActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class HomePageActivity extends AppCompatActivity {
+public class LoginPageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
