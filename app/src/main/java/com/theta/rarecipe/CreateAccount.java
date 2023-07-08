3f56a726db66603cb454a19f01811bc1c492a145
DyeDrake <aboateng_clifford@st.ug.edu.gh>
2023-07-08 22:30:06 +0000

    almost there

--- app/src/main/java/com/theta/rarecipe/CreateAccount.java
+++ app/src/main/java/com/theta/rarecipe/CreateAccount.java
@@ -2,7 +2,10 @@ package com.theta.rarecipe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CreateAccount extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class CreateAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        Button signUpButton = findViewById(R.id.signIn_Btn);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CreateAccount.this, SignInActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
