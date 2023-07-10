c92e46697d2165f9c07204e9ebdfe1455dbac31a
livingstone234 <seblelivingstone@gmail.com>
2023-07-10 15:04:26 +0100

    Corrected an Error

--- app/src/main/java/com/theta/rarecipe/SearchScreen.java
+++ app/src/main/java/com/theta/rarecipe/SearchScreen.java
@@ -20,31 +20,11 @@ public class SearchScreen extends AppCompatActivity {
     private RecyclerView recentRecipesRecyclerView;
     private List<FoodItem> foodItemList;
 
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_result);
 
-        searchView = findViewById(R.id.search_view);
-        searchView.setIconifiedByDefault(false);
-        searchView.setQueryHint("Search recipe");
 
-        Intent intent = getIntent();
-//        foodItemList = intent.getParcelableArrayListExtra("foodItemList");
-        setupRecyclerViews();
 
     }
-    private void setupRecyclerViews() {
-        popularCategoryRecyclerView = findViewById(R.id.popular_section_recycler);
-        popularCategoryRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
-
-        recentRecipesRecyclerView = findViewById(R.id.recent_recipes_recycler);
-        recentRecipesRecyclerView.setLayoutManager(new GridLayoutManager(this, 1));
-
-        PopularFoodAdapter popularFoodAdapter = new PopularFoodAdapter(foodItemList);
-        popularCategoryRecyclerView.setAdapter(popularFoodAdapter);
-
-        RecentFoodAdapter recentFoodAdapter = new RecentFoodAdapter(foodItemList);
-        recentRecipesRecyclerView.setAdapter(recentFoodAdapter);
-    }
-
 }
