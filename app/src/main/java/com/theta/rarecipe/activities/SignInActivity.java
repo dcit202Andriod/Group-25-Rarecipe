dbda996eaeae96136f25e643a20cd3d12cdb3341
DyeDrake <aboateng_clifford@st.ug.edu.gh>
2023-07-10 19:01:25 +0000

    Sign Up button given clickable fucntionality to Create account page

--- app/src/main/java/com/theta/rarecipe/activities/SignInActivity.java
+++ app/src/main/java/com/theta/rarecipe/activities/SignInActivity.java
@@ -2,7 +2,10 @@ package com.theta.rarecipe.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.theta.rarecipe.R;
 
@@ -12,5 +15,14 @@ public class SignInActivity extends AppCompatActivity {
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.sign_in_activity);
+
+            TextView signUpText = findViewById(R.id.signUpText);
+            signUpText.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(SignInActivity.this, CreateAccount.class);
+                    startActivity(intent);
+                }
+            });
         }
     }
